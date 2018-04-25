31887cbb8731c23050abe7b41152911d274b2b97
abbotteverett <severettabbott@gmail.com>
2018-04-25 16:19:34 -0700

    Added a menu bar to the top of the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react'
 import About from './About';
 import Contact from './Contact';
 import Portfolio from './Portfolio';
@@ -8,17 +9,25 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const views = [
+      { key: 'Home', name: 'Home' },
+      { key: 'About', name: 'About' },
+      { key: 'Contact', name: 'Contact' },
+      { key: 'Portfolio', name: 'Portfolio' },
+    ]
     return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/about" render={() => <About />} /> 
-            <Route path="/contact" render={() => <Contact />} /> 
-            <Route path="/portfolio" render={() => <Portfolio />} /> 
-            <Route path="/" render={() => <Home />} /> 
-          </Switch>
-        </div>
-      </Router>
+        <Router>
+          <div>
+            <Menu items={views} />
+            <Switch>
+              <Route path="/about" render={() => <About />} /> 
+              <Route path="/contact" render={() => <Contact />} /> 
+              <Route path="/portfolio" render={() => <Portfolio />} /> 
+              <Route path="/" render={() => <Home />} /> 
+            </Switch>
+          </div>
+          
+        </Router>
     );
   }
 }
